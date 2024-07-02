f26d304cdf9e7e54d4f34827cdd3b97dc0e91bd1
Saugat Rajbhandari <43225705+saugatrajbhandari@users.noreply.github.com>
2024-07-03 00:30:24 +0545

    add:promise

---
+++ promise.js
@@ -0,0 +1,38 @@
+const cart = ["shoes", "pant", "kurtha"];
+
+const promise = createOrder(cart);
+promise
+  .then((orderId) => {
+    console.log(orderId);
+    return orderId;
+  })
+  .then((orderId) => {
+    return proccedToPayment(orderId);
+  })
+  .then((paymentInfo) => {
+    console.log(paymentInfo);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+function createOrder(cart) {
+  return new Promise((resolve, reject) => {
+    if (!cart) {
+      reject(new Error("error in create order"));
+    }
+
+    const orderId = "213";
+    setTimeout(() => {
+      resolve(orderId);
+    }, 3000);
+  });
+}
+
+function proccedToPayment(orderId) {
+  return new Promise((resolve, reject) => {
+    resolve("payment successful", orderId);
+  });
+}
+
+console.log("hello");
